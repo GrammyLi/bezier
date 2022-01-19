59568e3ae5da25d63146ef2ae94ff0b423c4dc7c
魔法师 <grammyli@outlook.com>
2022-01-19 21:01:42 +0800

    fix: 显示顺序

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-19 17:11:18
- * @LastEditTime: 2022-01-19 19:09:33
+ * @LastEditTime: 2022-01-19 21:00:38
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /Bezier-master/main.js
@@ -20,10 +20,18 @@ const drawPoint = (canvas, point, size = 3, color = "black", isStrokeStyle = fal
 };
 
 const drawLine = (canvas, p1, p2) => {
-  for (let i = 0; i < 1; i += 0.01) {
+  for (let i = 0; i < 1; i += 0.001) {
     let p = vectorInterpolate(p1, p2, i);
     log("p", p);
     drawPoint(canvas, p, 3, 'pink', true);
+  }
+}
+
+const drawBezier = (canvas, start, end, p1, p2) => {
+  for (let i = 0; i < 1; i += 0.001) {
+    let p = bezier(start, end, p1, p2, i);
+    // log("p", p);
+    drawPoint(canvas, p);
   }
 }
 
@@ -32,23 +40,30 @@ const __main = () => {
 
   const start = [20, 200];
   const end = [10, 10];
-  drawPoint(canvas, start, 10, "red");
-  drawPoint(canvas, end, 10, "red");
+  
 
   const p1 = [350, 50];
   const p2 = [70, 90];
-  drawPoint(canvas, p1, 10, "blue");
-  drawPoint(canvas, p2, 10, "blue");
 
-  for (let i = 0; i < 1; i += 0.01) {
-    let p = bezier(start, end, p1, p2, i);
-    // log("p", p);
-    drawPoint(canvas, p);
-  }
+  drawBezier(canvas, start, end, p1, p2)
+
   log('start', start, 'p1', p1)
   drawLine(canvas, start, p1)
   drawLine(canvas, p1, p2)
   drawLine(canvas, p2, end)
+
+  drawPoint(canvas, start, 10, "red");
+  drawPoint(canvas, end, 10, "red");
+
+
+  drawPoint(canvas, p1, 10, "blue");
+  drawPoint(canvas, p2, 10, "blue");
+
+  // for (let i = 0; i < 1; i += 0.001) {
+  //   let p = bezier2(start, end, p1,  i);
+  //   // log("p", p);
+  //   drawPoint(canvas, p);
+  // }
 };
 
 __main();
--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-19 18:55:42
- * @LastEditTime: 2022-01-19 18:55:42
+ * @LastEditTime: 2022-01-19 20:58:38
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /bezier/utils.js
@@ -13,18 +13,27 @@ const vectorLength = (v) => {
   return Math.sqrt(x * x + y * y);
 };
 
+// B(t) = P0 + (P1 - P0) * t
 const interpolate = (a, b, factor) => {
   return a + (b - a) * factor;
 };
 
-const vectorInterpolate = (a, b, factor) => {
-  return [interpolate(a[0], b[0], factor), interpolate(a[1], b[1], factor)];
+const vectorInterpolate = (p1, p2, factor) => {
+  return [interpolate(p1[0], p2[0], factor), interpolate(p1[1], p2[1], factor)];
 };
 
+// 三
 const bezier = (start, end, p1, p2, factor) => {
   const c1 = vectorInterpolate(p1, p2, factor);
   const c2 = vectorInterpolate(start, c1, factor);
   const c3 = vectorInterpolate(c1, end, factor);
   const p = vectorInterpolate(c2, c3, factor);
+  return p;
+};
+
+// 二
+const bezier2 = (start, end, p1, factor) => {
+  const c1 = vectorInterpolate(start, p1, factor);
+  const p = vectorInterpolate(c1, end, factor);
   return p;
 };
