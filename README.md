2e3aa9dbc4b2a7c623775d1c3cb0a857297c7c14
魔法师 <grammyli@outlook.com>
2022-01-20 10:10:00 +0800

    refactor： main.js

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-01-19 18:56:22
- * @LastEditTime: 2022-01-19 19:36:05
+ * @LastEditTime: 2022-01-20 09:37:35
  * @LastEditors: Please set LastEditors
  * @Description:  
  * @FilePath: /bezier/README.md
@@ -9,6 +9,16 @@
 ### online bezier
 
 
+### Task
+
+- 画二阶、三阶
+  1. 起点、终点和确定其他点
+  2. 画线
+  3. 画曲线
+
+- 自定义起点、终点、其他点
+  1. 点击事件，确定点的类型
+  2. 清空画布，然后重新画曲线
 ### link
 
 https://bezier-taupe.vercel.app/
--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 /*
  * @Author: your name
  * @Date: 2022-01-19 17:11:18
- * @LastEditTime: 2022-01-19 21:00:38
+ * @LastEditTime: 2022-01-20 10:05:43
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /Bezier-master/main.js
  */
-const drawPoint = (canvas, point, size = 3, color = "black", isStrokeStyle = false) => {
+const drawPoint = (
+  canvas,
+  point,
+  size = 3,
+  color = "black",
+) => {
   const [x, y] = point;
   const ctx = canvas.getContext("2d");
   ctx.fillStyle = color;
-  if (isStrokeStyle) {
-    ctx.strokeStyle = color
-  }
+
+  ctx.strokeStyle = color;
+
   ctx.beginPath();
   ctx.arc(x, y, size / 2, 0, 2 * Math.PI);
   ctx.fill();
@@ -23,9 +28,9 @@ const drawLine = (canvas, p1, p2) => {
   for (let i = 0; i < 1; i += 0.001) {
     let p = vectorInterpolate(p1, p2, i);
     log("p", p);
-    drawPoint(canvas, p, 3, 'pink', true);
+    drawPoint(canvas, p, 3, "pink", true);
   }
-}
+};
 
 const drawBezier = (canvas, start, end, p1, p2) => {
   for (let i = 0; i < 1; i += 0.001) {
@@ -33,37 +38,42 @@ const drawBezier = (canvas, start, end, p1, p2) => {
     // log("p", p);
     drawPoint(canvas, p);
   }
-}
+};
+
+const drawBezier2 = (canvas, start, end, p1) => {
+  for (let i = 0; i < 1; i += 0.001) {
+    let p = bezier2(start, end, p1, i);
+    // log("p", p);
+    drawPoint(canvas, p);
+  }
+};
+
+const drawPoints = (canvas, start, end, p1, p2) => {
+  drawPoint(canvas, start, 10, "red");
+  drawPoint(canvas, end, 10, "red");
+
+  drawPoint(canvas, p1, 10, "blue");
+  drawPoint(canvas, p2, 10, "blue");
+};
+
+const drawLines = (canvas, start, end, p1, p2) => {
+  drawLine(canvas, start, p1);
+  drawLine(canvas, p1, p2);
+  drawLine(canvas, p2, end);
+};
 
 const __main = () => {
   const canvas = document.querySelector(".g-canvas");
 
   const start = [20, 200];
   const end = [10, 10];
-  
 
   const p1 = [350, 50];
   const p2 = [70, 90];
 
-  drawBezier(canvas, start, end, p1, p2)
-
-  log('start', start, 'p1', p1)
-  drawLine(canvas, start, p1)
-  drawLine(canvas, p1, p2)
-  drawLine(canvas, p2, end)
-
-  drawPoint(canvas, start, 10, "red");
-  drawPoint(canvas, end, 10, "red");
-
-
-  drawPoint(canvas, p1, 10, "blue");
-  drawPoint(canvas, p2, 10, "blue");
-
-  // for (let i = 0; i < 1; i += 0.001) {
-  //   let p = bezier2(start, end, p1,  i);
-  //   // log("p", p);
-  //   drawPoint(canvas, p);
-  // }
+  drawLines(canvas, start, end, p1, p2);
+  drawBezier(canvas, start, end, p1, p2);
+  drawPoints(canvas, start, end, p1, p2);
 };
 
 __main();
