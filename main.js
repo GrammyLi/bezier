3805e80cca3c0dca2ba30f92659dce9fb8f9f6fc
魔法师 <grammyli@outlook.com>
2022-01-20 17:04:34 +0800

    feat: n 阶贝塞尔曲线

--- main.js
+++ main.js
@@ -1,45 +1,11 @@
 /*
  * @Author: your name
  * @Date: 2022-01-19 17:11:18
- * @LastEditTime: 2022-01-20 10:05:43
+ * @LastEditTime: 2022-01-20 17:03:47
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /Bezier-master/main.js
  */
-const drawPoint = (
-  canvas,
-  point,
-  size = 3,
-  color = "black",
-) => {
-  const [x, y] = point;
-  const ctx = canvas.getContext("2d");
-  ctx.fillStyle = color;
-
-  ctx.strokeStyle = color;
-
-  ctx.beginPath();
-  ctx.arc(x, y, size / 2, 0, 2 * Math.PI);
-  ctx.fill();
-  ctx.stroke();
-};
-
-const drawLine = (canvas, p1, p2) => {
-  for (let i = 0; i < 1; i += 0.001) {
-    let p = vectorInterpolate(p1, p2, i);
-    log("p", p);
-    drawPoint(canvas, p, 3, "pink", true);
-  }
-};
-
-const drawBezier = (canvas, start, end, p1, p2) => {
-  for (let i = 0; i < 1; i += 0.001) {
-    let p = bezier(start, end, p1, p2, i);
-    // log("p", p);
-    drawPoint(canvas, p);
-  }
-};
-
 const drawBezier2 = (canvas, start, end, p1) => {
   for (let i = 0; i < 1; i += 0.001) {
     let p = bezier2(start, end, p1, i);
@@ -48,18 +14,44 @@ const drawBezier2 = (canvas, start, end, p1) => {
   }
 };
 
-const drawPoints = (canvas, start, end, p1, p2) => {
+const drawBezier3 = (canvas, start, end, p1, p2) => {
+  for (let i = 0; i < 1; i += 0.001) {
+    let p = bezier3(start, end, p1, p2, i);
+    // log("p", p);
+    drawPoint(canvas, p);
+  }
+};
+
+const drawPoints = (canvas, positions) => {
+  const startIndex = 0
+  const endIndex = positions.length - 1
+  const start = positions[startIndex]
+  const end = positions[endIndex]
   drawPoint(canvas, start, 10, "red");
   drawPoint(canvas, end, 10, "red");
 
-  drawPoint(canvas, p1, 10, "blue");
-  drawPoint(canvas, p2, 10, "blue");
+  positions.forEach((p, i) => {
+    if ([startIndex, endIndex].includes(i)) {
+      return
+    }
+    drawPoint(canvas, p, 10, "blue");
+  });
 };
 
-const drawLines = (canvas, start, end, p1, p2) => {
-  drawLine(canvas, start, p1);
-  drawLine(canvas, p1, p2);
-  drawLine(canvas, p2, end);
+const drawBezier = (canvas, positions) => {
+  for (let i = 0; i < 1; i += 0.001) {
+    let p = bezier(positions, i)[0]
+    drawPoint(canvas, p);
+  }
+}
+
+const drawLines = (canvas, positions) => {
+  positions.forEach((p, i) => {
+    if (positions.length - 1 === i) {
+      return
+    }
+    drawLine(canvas, p, positions[i + 1]);
+  });
 };
 
 const __main = () => {
@@ -68,12 +60,14 @@ const __main = () => {
   const start = [20, 200];
   const end = [10, 10];
 
-  const p1 = [350, 50];
-  const p2 = [70, 90];
+  const p1 = [250, 50];
+  const p2 = [80, 80];
 
-  drawLines(canvas, start, end, p1, p2);
-  drawBezier(canvas, start, end, p1, p2);
-  drawPoints(canvas, start, end, p1, p2);
+  const ps = [start, p1, p2, end]
+  
+  drawLines(canvas, ps)
+  drawPoints(canvas, ps)
+  drawBezier(canvas, ps)
 };
 
 __main();
--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-19 18:55:42
- * @LastEditTime: 2022-01-19 20:58:38
+ * @LastEditTime: 2022-01-20 16:52:40
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /bezier/utils.js
@@ -18,22 +18,64 @@ const interpolate = (a, b, factor) => {
   return a + (b - a) * factor;
 };
 
+// 一
 const vectorInterpolate = (p1, p2, factor) => {
   return [interpolate(p1[0], p2[0], factor), interpolate(p1[1], p2[1], factor)];
-};
-
-// 三
-const bezier = (start, end, p1, p2, factor) => {
-  const c1 = vectorInterpolate(p1, p2, factor);
-  const c2 = vectorInterpolate(start, c1, factor);
-  const c3 = vectorInterpolate(c1, end, factor);
-  const p = vectorInterpolate(c2, c3, factor);
-  return p;
 };
 
 // 二
 const bezier2 = (start, end, p1, factor) => {
   const c1 = vectorInterpolate(start, p1, factor);
-  const p = vectorInterpolate(c1, end, factor);
+  const c2 = vectorInterpolate(p1, end, factor);
+  const p = vectorInterpolate(c1, c2, factor);
   return p;
+};
+
+// 三
+const bezier3 = (start, end, p1, p2, factor) => {
+  const c1 = vectorInterpolate(start, p1, factor);
+  const c2 = vectorInterpolate(p1, p2, factor);
+  const c3 = vectorInterpolate(p2, end, factor);
+
+  return bezier2(c1, c3, c2, factor);
+};
+
+const bezier = (positions, factor) => {
+  while (positions.length > 1) {
+    const newPostions = [];
+    positions.forEach((item, index) => {
+      if (index === positions.length - 1) {
+        return;
+      }
+      let p = vectorInterpolate(item, positions[index + 1], factor);
+      newPostions.push(p);
+    });
+    return bezier(newPostions, factor);
+  }
+  return positions;
+};
+
+const drawPoint = (
+  canvas,
+  point,
+  size = 3,
+  color = "black",
+) => {
+  const [x, y] = point;
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = color;
+
+  ctx.strokeStyle = color;
+
+  ctx.beginPath();
+  ctx.arc(x, y, size / 2, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.stroke();
+};
+
+const drawLine = (canvas, p1, p2) => {
+  for (let i = 0; i < 1; i += 0.001) {
+    let p = vectorInterpolate(p1, p2, i);
+    drawPoint(canvas, p, 3, "pink", true);
+  }
 };
